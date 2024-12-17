79cf5fbe152d6ee263941b806ab2ac98f2c5964a
Ainun-Nadjib17 <mokhamad.ainun32@smk.belajar.id>
2024-12-17 11:30:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Animasi scroll untuk bagian tertentu saat menu ditekan
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
